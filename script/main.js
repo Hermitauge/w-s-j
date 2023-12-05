6b7fdf8ee6f3985b2bb5ae79ef2128a1c10db7ff
mehnz <>
2023-12-05 04:22:15 -0800

    update

--- script/main.js
+++ script/main.js
@@ -470,12 +470,12 @@ if (openPanel) {
                   
                   // Extract URL and modify it if it's not null
                   let videoURL = videoElement.textContent.trim();
-                  let modifiedVideoURL = videoURL ? videoURL.replace('500/500', '360/360/autoplay') : '';
+                  let modifiedVideoURL = videoURL ? videoURL.replace('500/500', '400/400/autoplay') : '';
               
                   if (modifiedVideoURL) {
                       iframe.src = modifiedVideoURL;
-                      iframe.width = '360';
-                      iframe.height = '360';
+                      iframe.width = '400';
+                      iframe.height = '400';
                       iframe.frameBorder = '0';
                       iframe.allow = 'autoplay; encrypted-media';
                       iframe.allowFullscreen = true;
