cf26f9292fcba7352c8e6ae002791d6dff7f3525
mehnz <>
2023-12-07 12:11:32 -0800

    update

--- script/main.js
+++ script/main.js
@@ -203,9 +203,11 @@ async function fetchProductsForFilters(checkedShapes, minPrice, maxPrice, minCar
     function mapSliderValue(mapping, value) {
       return mapping[Math.min(Math.max(parseInt(value), 0), mapping.length - 1)];
   }
-  
+  let offset = 0;
+  const limit = 20;
+
   async function fetchProducts(shapeFilter = '', minPrice = '', maxPrice = '', minCarats = '', maxCarats = '', minColor = '', maxColor = '', minClarity = '', maxClarity = '', minCut = '', maxCut = '', checkedLabs = [], minPolish = '', maxPolish = '', minSymmetry = '', maxSymmetry = '', minFluor = '', maxFluor = '', minTable = '', maxTable = '', minDepth = '', maxDepth = '', minRatio = '', maxRatio = '', checkedOrigin = []) {  
-      const url = new URL('https://57urluwych.execute-api.us-west-1.amazonaws.com/live/diamonds');  
+      const url = new URL('https://57urluwych.execute-api.us-west-1.amazonaws.com/live/diamonds?offset=${offset}&limit=${limit}');  
   
       const setUrlParam = (param, value) => {
           if (value) url.searchParams.set(param, value);
