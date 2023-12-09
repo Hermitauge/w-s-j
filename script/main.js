8d9a7a4c580c6a257273b3ca1cae2e275e50120f
mehnz <>
2023-12-08 16:22:54 -0800

    update

--- script/main.js
+++ script/main.js
@@ -36,8 +36,35 @@ import { showLoadingAnimation, hideLoadingAnimation } from 'https://cdn.jsdelivr
                 fetching = true;
                 showLoadingAnimation();
       
-                const scrollFetched = await fetchProducts(checkedShapes, minPrice, maxPrice, minCarats, maxCarats, minColor, maxColor, minClarity, maxClarity, minCut, maxCut, checkedLabs, minPolish, maxPolish, minSymmetry, maxSymmetry, minFluor, maxFluor, minTable, maxTable, minDepth, maxDepth, minRatio, maxRatio, checkedOrigin, offset, limit);
-      
+                const scrollFetched = await fetchProductsForFilters(
+                    checkedShapes, 
+                    '', // Replace with the current minPrice if needed
+                    '', // Replace with the current maxPrice if needed
+                    '', // Replace with the current minCarats if needed
+                    '', // Replace with the current maxCarats if needed
+                    '', // Replace with the current minColor if needed
+                    '', // Replace with the current maxColor if needed
+                    '', // Replace with the current minClarity if needed
+                    '', // Replace with the current maxClarity if needed
+                    '', // Replace with the current minCut if needed
+                    '', // Replace with the current maxCut if needed
+                    checkedLabs, 
+                    '', // Replace with the current minPolish if needed
+                    '', // Replace with the current maxPolish if needed
+                    '', // Replace with the current minSymmetry if needed
+                    '', // Replace with the current maxSymmetry if needed
+                    '', // Replace with the current minFluor if needed
+                    '', // Replace with the current maxFluor if needed
+                    '', // Replace with the current minTable if needed
+                    '', // Replace with the current maxTable if needed
+                    '', // Replace with the current minDepth if needed
+                    '', // Replace with the current maxDepth if needed
+                    '', // Replace with the current minRatio if needed
+                    '', // Replace with the current maxRatio if needed
+                    checkedOrigin, 
+                    offset, 
+                    limit
+                  );
                 hideLoadingAnimation();
                 await updateList(scrollFetched, listInstance, itemTemplateElement, true);
                 offset += limit;
