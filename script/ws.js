399741a4be276f191c0279c248b8224ade55a3ff
Hermitauge <32431973+Hermitauge@users.noreply.github.com>
2024-01-28 14:37:02 -0800

    added stripe

--- script/ws.js
+++ script/ws.js
@@ -1,7 +1,7 @@
 import { formatShape, formatPrice, formatCarats, formatLength, formatWidth, formatDepth, formatTable, formatCut, formatDiamondIcon } from 'https://cdn.jsdelivr.net/gh/Hermitauge/W-S@873a511bc3a68ec8a96d90ce3b9fa32fb5359102/script/formatData.js';
 import { showLoadingAnimation, hideLoadingAnimation, debounce } from 'https://cdn.jsdelivr.net/gh/Hermitauge/W-S@6b30df73e525982e0bece6ec0701b74f216a7b00/script/loadingAnimation.js';
 import { handlePanelClick, processVideo, bindProductDataToElement } from 'https://cdn.jsdelivr.net/gh/Hermitauge/w-s-j@6d1100acf8f6fbe6ff65ee144de5e56dee5ed3fb/script/utils.js';
-import { StripeHandler } from 'https://cdn.jsdelivr.net/gh/Hermitauge/w-s-j@18b01241bf39462d5ed905935fda988f71bbae74/script/stripeHandler.js';
+import { StripeHandler } from 'https://cdn.jsdelivr.net/gh/Hermitauge/w-s-j@665ccf547d763427695bf24255b459435e80ce31/script/stripeHandler.js';
 class DiamondCollection {
   constructor() {
     console.log('DiamondCollection constructor called');
@@ -27,7 +27,7 @@ class DiamondCollection {
       this.bindEvents();
       this.clearAndFetchFilteredProducts(false); // Prevent URL update on initial load
     }    
-         this.stripeHandler = new StripeHandler('YOUR_STRIPE_PUBLIC_KEY');
+         this.stripeHandler = new StripeHandler('pk_live_BWTYy9jFuYJT2w2uxmwjCNTs00BNT5lIDz');
     }
         
   bindTabButtons() {
