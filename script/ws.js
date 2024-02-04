2501a04ed821da8f3e1cf58a8604b7a6f51f292a
mehnz <>
2024-02-04 06:06:04 -0800

    update

--- script/ws.js
+++ script/ws.js
@@ -1,6 +1,6 @@
 import { formatShape, formatPrice, formatCarats, formatLength, formatWidth, formatDepth, formatTable, formatCut, formatDiamondIcon } from 'https://cdn.jsdelivr.net/gh/Hermitauge/W-S@873a511bc3a68ec8a96d90ce3b9fa32fb5359102/script/formatData.js';
 import { showLoadingAnimation, hideLoadingAnimation, debounce } from 'https://cdn.jsdelivr.net/gh/Hermitauge/W-S@6b30df73e525982e0bece6ec0701b74f216a7b00/script/loadingAnimation.js';
-import { handlePanelClick, processVideo, bindProductDataToElement } from 'https://cdn.jsdelivr.net/gh/Hermitauge/w-s-j@497a063b0c5922c4ea31a1441dabdc84e36c0c5f/script/utils.js';
+import { handlePanelClick, processVideo, bindProductDataToElement } from 'https://github.com/Hermitauge/w-s-j/blob/210973d9ef326e8e5482bcbd860d969f031bdd90/script/utils.js';
 
 class DiamondCollection {
   constructor() {
